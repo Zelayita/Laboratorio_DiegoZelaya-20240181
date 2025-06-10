e9b81386f1f0de29411b1171616bd40ac776f3b3
Zelayita <20240181@ricaldone.edu.sv>
2025-06-10 14:00:02 -0600

    "Segundo Commit sobre Como empeza todo"

--- Laboratorio_DiegoZelaya-20240181/src/main/java/Laboratorio_DiegoZelaya20240181/Main.java
+++ Laboratorio_DiegoZelaya-20240181/src/main/java/Laboratorio_DiegoZelaya20240181/Main.java
@@ -1,8 +1,17 @@
 package Laboratorio_DiegoZelaya20240181;
 
 
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class Main {
     public static void main(String[] args) {
-        
+        TreeMap<Integer, String> contactos = new TreeMap<>();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("*******Bienvenidos a Agenda de Contactos*******");
+        System.out.println("\n Que es lo primero que desea hacer? (Presione el numero 😁)");
+        System.out.println("1.Agregar Contacto || 2.Mostrar Contactos Ordenados || 3.Buscar Contacto por Nombre || 4. Eliminar Contacto || 5.Salir");
+
     }
 }
